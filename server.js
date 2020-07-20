71d9f2497c45e60b538ff23b82d77054732db8ce
Shaun Hamilton <2397860h@student.gla.ac.uk>
2020-07-20 17:53:34 +0000

    add changes for fcc-tests

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
-// require and use "multer"...
-
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -17,6 +17,11 @@ app.get('/', function (req, res) {
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+app.route('/api/fileanalyse').post(upload.single('upfile'), (req,res)=> {
+  console.log(req.body, req.file)
+  res.json({filename: req.file.filename, size: req.file.size});
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
